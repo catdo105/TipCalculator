32baf09db6ee7e2c73843f1afc33a129bbd0b940
Catherine Do <catdo105@gmail.com>
2021-02-25 20:39:24 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Catherine Do on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
